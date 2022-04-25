04357a51a3d74bba0475140b13c0bc0fb90dd448
indri <indri@INDRIT-VAKA>
2022-04-25 19:18:45 +0200

    Add project files.

---
+++ HttpClientDemo/Models/DayForecastModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HttpClientDemo.Models
+{
+    public class DayForecastModel
+    {
+        public long Id { get; set; }
+        [JsonPropertyName("weather_state_name")]
+        public string? WeatherStateName { get; set; }
+        [JsonPropertyName("applicable_date")]
+        public string? ApplicableDate { get; set; }
+        [JsonPropertyName("min_temp")]
+        public float MinTemp { get; set; }
+        [JsonPropertyName("max_temp")]
+        public float MaxTemp { get; set; }
+    }
+
+
+
+}
---
+++ HttpClientDemo/Models/WatherForecastModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HttpClientDemo.Models
+{
+
+
+    public class WatherForecastModel
+    {
+        [JsonPropertyName("Consolidated_weather")]
+        public DayForecastModel[] ConsolidatedWeather { get; set; }
+        public DateTime Time { get; set; }
+        [JsonPropertyName("Sun_rise")]
+        public DateTime SunRise { get; set; }
+        [JsonPropertyName("Sun_set")]
+        public DateTime SunSet { get; set; }
+        [JsonPropertyName("Timezone_name")]
+        public string? TimezoneName { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Timezone { get; set; }
+    }
+
+}
